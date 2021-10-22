bbc7441eca9314c06d43d239ad68a79a8587f9ed
Santiago Solis <santux09@gmail.com>
2021-10-22 15:33:46 -0300

    added metropolis font

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import MessageSection from '../../components/MessageSection'
 import './Home.scss'
 function Home() {
     return (
         <div className="HomeLayout">
             <div className="HomeLayout__sidebar">Sidebar</div>
-            <div className="HomeLayout__messages">Messages</div>
+            <div className="HomeLayout__messages"><MessageSection /></div>
             <div className="HomeLayout__chat">Chat</div>
             <div className="HomeLayout__directory">Directory</div>
         </div>
