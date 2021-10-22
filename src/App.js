01fcb3207869d54ef05c789671eec140df4a7ebe
Santiago Solis <ssolis@kubikware.com>
2021-10-22 11:50:42 -0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
 function App(){
-   return <div> Flydevs - CHATAPP </div>
+   return (
+   <Router>
+       <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+              <About/>
+            </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+        </Switch>
+        </div>
+  </Router>
+  )
 }
 export default App;
