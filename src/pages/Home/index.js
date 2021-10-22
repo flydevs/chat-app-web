7cfc8eea581e3a6cd361a4b7bbdfd5c82aa5c978
Santiago Solis <ssolis@kubikware.com>
2021-10-22 13:57:46 -0300

    structured

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="HomeLayout">
+            <div className="HomeLayout__sidebar">Sidebar</div>
+            <div className="HomeLayout__messages">Messages</div>
+            <div className="HomeLayout__chat">Chat</div>
+            <div className="HomeLayout__directory">Directory</div>
+        </div>
+    )
+}
+
+export default Home
