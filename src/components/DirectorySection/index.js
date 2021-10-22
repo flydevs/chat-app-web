f99f5e3a54c1ac2e9b9f96649787d4072daa65b4
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2021-10-22 17:12:07 -0300

    directory scss layout done

--- src/components/DirectorySection/index.js
+++ src/components/DirectorySection/index.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import styles from './directorySection.scss'
+import React from "react";
+import "./directorySection.scss";
 
 function DirectorySection() {
-    return (
-        <div className="DirectoryCont">
-            <div className="DirectoryCont__head">
-                <h2>Directory</h2>
-                <div>...</div>
-            </div>
-            <div className="DirectoryCont__main1">
-                <div className="DirectoryCont__main1__flex">
-                    <h4>Team Members</h4>
-                    <div>6</div>
-                </div>
-                <h1>cards will display here</h1>
-            </div>
-            <div className="DirectoryCont__main2">
-                <div className="DirectoryCont__main2__flex">
-                <h4>Files</h4>
-                <div>125</div>
-                </div>
-                <h1>Files willl be desplayed here</h1>
-            </div>
-            
+  return (
+    <div className="DirectoryCont">
+      <div className="DirectoryCont__head">
+        <h2>Directory</h2>
+        <div>
+          <p>...</p>
         </div>
-    )
+      </div>
+      <div className="DirectoryCont__main">
+        <div className="DirectoryCont__main__flex">
+          <h4>Team Members</h4>
+          <div>6</div>
+        </div>
+        <h1>cards will display here</h1>
+      </div>
+      <div className="DirectoryCont__main">
+        <div className="DirectoryCont__main__flex">
+          <h4>Files</h4>
+          <div>125</div>
+        </div>
+        <h1>Files willl be desplayed here</h1>
+      </div>
+    </div>
+  );
 }
 
-export default DirectorySection
+export default DirectorySection;
