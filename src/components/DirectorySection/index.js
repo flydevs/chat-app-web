d10a910c58aca47719aafec1347c318486f7182b
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2021-10-25 09:53:40 -0300

    created member cards component and changed the directory '...' element from div to button

--- src/components/DirectorySection/index.js
+++ src/components/DirectorySection/index.js
@@ -1,21 +1,29 @@
 import React from "react";
+import MembersCards from "../MembersCards";
 import "./directorySection.scss";
+import { HiDotsVertical } from "react-icons/hi";
 
 function DirectorySection() {
   return (
     <div className="DirectoryCont">
       <div className="DirectoryCont__head">
         <h2>Directory</h2>
-        <div>
-          <p>...</p>
-        </div>
+        <button className="DirectoryCont__head__more">
+          <HiDotsVertical className="DirectoryCont__head__more__icon" />
+        </button>
       </div>
       <div className="DirectoryCont__main">
         <div className="DirectoryCont__main__flex">
           <h4>Team Members</h4>
           <div>6</div>
         </div>
-        <h1>cards will display here</h1>
+        <div className="DirectoryCont__main__cards">
+          <MembersCards />
+          <MembersCards />
+          <MembersCards />
+          <MembersCards />
+          <MembersCards />
+        </div>
       </div>
       <div className="DirectoryCont__main">
         <div className="DirectoryCont__main__flex">
@@ -29,3 +37,5 @@ function DirectorySection() {
 }
 
 export default DirectorySection;
+{
+}
