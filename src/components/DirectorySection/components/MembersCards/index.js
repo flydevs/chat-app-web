f93232c8eef5c4b2243252cba3e6d21dd9945ce5
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2021-10-25 17:42:06 -0300

    replaced avatar pic with avatar component on members card

--- src/components/DirectorySection/components/MembersCards/index.js
+++ src/components/DirectorySection/components/MembersCards/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./MembersCards.scss";
+import Avatar from "../../../common/Avatar/Avatar";
 
 const MembersCards = () => {
   return (
     <div className="MemberCard">
-      <div className="MemberCard__ImgContainer">
-        <img
-          className="MemberCard__ImgContainer__ProfilePic"
-          src={`https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
-        />
-      </div>
+      <Avatar
+        profileImg={`https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+        size={48}
+      />
       <div className="MemberCard__InfoFlex">
         <h5>Florencio Dorrance</h5>
         <p>Market Development Manager</p>
