b1603962146a7be593a03948c5dfa234e4bdb208
Tinchoip <martinmontenegro93@gmail.com>
2021-10-26 10:13:18 -0300

    header advances

--- src/components/ChatSection/components/ChatBody/ChatBody.js
+++ src/components/ChatSection/components/ChatBody/ChatBody.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function ChatBody() {
     return(
-        <div>HELLO Body</div>
+        <div></div>
     )
 }
 
--- src/components/ChatSection/components/ChatHeader/ChatHeader.js
+++ src/components/ChatSection/components/ChatHeader/ChatHeader.js
@@ -1,9 +1,26 @@
 import "./ChatHeader.scss";
 import React from "react";
+import Avatar from "../../../common/Avatar/Avatar";
+import {BsFillTelephoneFill} from "react-icons/bs";
 
 function ChatHeader() {
     return(
-        <div>HELLO Header</div>
+        <div className="headerContainer">
+            <div className="profileContainer">
+            <Avatar size={40} profileImg={`https://image.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg`} />
+            <div className="nameStatusContainer">
+                <h2 className="chatTitle">Florencio Dorrance</h2>
+                <div className="chatStatusContainer">
+                    <div className="statusDot"></div>
+                    <p className="status">Online</p>
+                </div>
+            </div>
+            </div>
+            <div className="callContainer">
+                <BsFillTelephoneFill size={18} color={"#615EF0"}/>
+                <p className="call">Call</p>
+            </div>
+        </div>
     )
 }
 
