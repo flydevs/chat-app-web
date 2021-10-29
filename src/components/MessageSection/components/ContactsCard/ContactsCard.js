3f8de9732d2d8270b64adcd3220f433315a5e481
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2021-10-29 17:23:52 -0300

    created conversations context, fetching fake data, updating context and passing that to contact cards

--- src/components/MessageSection/components/ContactsCard/ContactsCard.js
+++ src/components/MessageSection/components/ContactsCard/ContactsCard.js
@@ -1,30 +1,30 @@
-
 import React from "react";
 import Avatar from "../../../common/Avatar/Avatar";
 import "./contactsCard.scss";
 function ContactsCard({
   profileImg,
-  contactName,
+  firstName,
+  lastName,
   lastMessage,
   badges,
   selected,
-  timeAgo,
+  timeAgo
 }) {
   console.log(badges);
   return (
     <div className={"contactCard " + selected}>
       <Avatar size={48} profileImg={profileImg} />
       <div className="contactCard__info">
-        <h2>{contactName}</h2>
+        <h2>{firstName + lastName}</h2>
         <h3>{lastMessage}</h3>
         <div className="contactCard__info__badges">
-          {badges.map((badge,i) => (
+          {badges.map((badge, i) => (
             <div
-            key={i}
+              key={i}
               className="contactCard__info__badges__badge"
               style={{
                 backgroundColor: badge.backgroundColor,
-                color: badge.color,
+                color: badge.color
               }}
             >
               {badge.text}
